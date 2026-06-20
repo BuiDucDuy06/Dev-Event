ce122153826b9e7189b6cd17077a0ce0d9bb801f
BuiDucDuy06 <buiduy2203@gmail.com>
2026-06-20 16:35:27 +0700

    fix: cant deploy

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,5 @@
 import EventsClient from './EventsClient';
 
-export const dynamic = "force-dynamic";
-
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
 async function getEvents() {
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,7 @@ import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRay";
 import Navbar from "@/components/Navbar";
 import AuthProvider from "@/components/AuthProvider";
-
-export const dynamic = "force-dynamic";
+import { Suspense } from "react";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -35,30 +34,31 @@ export default function RootLayout({
       lang="en"
       >
       <body className={cn("min-h-screen", "h-full", "antialiased", schibstedGrotesk.variable, martianMono.variable, "font-sans", geist.variable)}>
-        <AuthProvider>
-          <Navbar/>
-          <div className="absolute inset-0 top-0 -z-10 min-h-screen pointer-events-none">
-            <LightRays
-              raysOrigin="top-center-offset"
-              raysColor="#5dfeca"
-              raysSpeed={0.5}
-              lightSpread={0.9}
-              rayLength={1.4}
-              followMouse={true}
-              mouseInfluence={0.02}
-              noiseAmount={0}
-              distortion={0.01}
-              className="custom-rays"
-              pulsating={false}
-              fadeDistance={1}
-              saturation={1}
-            />
-          </div>
-
-          <main>
-            {children}
-          </main>
-        </AuthProvider>
+        <Suspense>
+          <AuthProvider>
+            <Navbar/>
+            <div className="absolute inset-0 top-0 -z-10 min-h-screen pointer-events-none">
+              <LightRays
+                raysOrigin="top-center-offset"
+                raysColor="#5dfeca"
+                raysSpeed={0.5}
+                lightSpread={0.9}
+                rayLength={1.4}
+                followMouse={true}
+                mouseInfluence={0.02}
+                noiseAmount={0}
+                distortion={0.01}
+                className="custom-rays"
+                pulsating={false}
+                fadeDistance={1}
+                saturation={1}
+              />
+            </div>
+            <main>
+              {children}
+            </main>
+          </AuthProvider>
+        </Suspense>
         </body>
     </html>
   );
